21accfffd67bc6925460ae62e2b9184172250de0
= <=>
2024-11-21 10:55:39 +0500

    javascript operators

---
+++ javascript lecture/js/operators.js
@@ -0,0 +1,52 @@
+// arithmetic operator
+
+// +, -, *, /, %
+
+// const num1 = 4;
+// const num2 = 9;
+
+// user input
+
+const num1 = parseInt(prompt("Enter first number"));
+const num2 = parseInt(prompt("Enter Second number"));
+
+document.write("num1 :" + num1 + "<br>");
+document.write("num2 :" + num2 + "<br>");
+
+// string + string
+// plus sign will use as a sign of concatenation
+document.write("2" + "4" + "<br>");
+
+// string + number
+// plus sign will use as a sign of concatenation
+document.write("2" + 4 + "<br>");
+
+// number + number
+// plus sign will use as a sign of addition
+document.write(2 + 4 + "<br>");
+
+
+let result = num1 + num2;
+document.write("the addition is :" + result + "<br>");
+
+
+// subtraction
+
+result = num1 - num2;
+document.write("the subtraction is :" + result + "<br>");
+
+// multiplication
+
+result = num1 * num2;
+document.write("the multiplication is :" + result + "<br>");
+
+// division
+
+result = num1 / num2;
+document.write("the division is :" + result + "<br>");
+
+// modulus
+
+result = num1 % num2;
+document.write("the modulus is :" + result + "<br>");
+
