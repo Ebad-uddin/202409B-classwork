93a62708e112130ec2526b04078d6dd85569c505
= <=>
2024-11-23 10:57:47 +0500

    done till javascript functinos

--- javascript lecture/js/operators.js
+++ javascript lecture/js/operators.js
@@ -7,46 +7,89 @@
 
 // user input
 
-const num1 = parseInt(prompt("Enter first number"));
-const num2 = parseInt(prompt("Enter Second number"));
+// const num1 = parseInt(prompt("Enter first number"));
+// const num2 = parseInt(prompt("Enter Second number"));
 
-document.write("num1 :" + num1 + "<br>");
-document.write("num2 :" + num2 + "<br>");
+// document.write("num1 :" + num1 + "<br>");
+// document.write("num2 :" + num2 + "<br>");
 
-// string + string
-// plus sign will use as a sign of concatenation
-document.write("2" + "4" + "<br>");
+// // string + string
+// // plus sign will use as a sign of concatenation
+// document.write("2" + "4" + "<br>");
 
-// string + number
-// plus sign will use as a sign of concatenation
-document.write("2" + 4 + "<br>");
+// // string + number
+// // plus sign will use as a sign of concatenation
+// document.write("2" + 4 + "<br>");
 
-// number + number
-// plus sign will use as a sign of addition
-document.write(2 + 4 + "<br>");
-
-
-let result = num1 + num2;
-document.write("the addition is :" + result + "<br>");
+// // number + number
+// // plus sign will use as a sign of addition
+// document.write(2 + 4 + "<br>");
 
 
-// subtraction
+// let result = num1 + num2;
+// document.write("the addition is :" + result + "<br>");
 
-result = num1 - num2;
-document.write("the subtraction is :" + result + "<br>");
 
-// multiplication
+// // subtraction
 
-result = num1 * num2;
-document.write("the multiplication is :" + result + "<br>");
+// result = num1 - num2;
+// document.write("the subtraction is :" + result + "<br>");
 
-// division
+// // multiplication
 
-result = num1 / num2;
-document.write("the division is :" + result + "<br>");
+// result = num1 * num2;
+// document.write("the multiplication is :" + result + "<br>");
 
-// modulus
+// // division
 
-result = num1 % num2;
-document.write("the modulus is :" + result + "<br>");
+// result = num1 / num2;
+// document.write("the division is :" + result + "<br>");
+
+// // modulus
+
+// result = num1 % num2;
+// document.write("the modulus is :" + result + "<br>");
+
+
+// javascript predefined functions
+
+// prompt(), alert(), confirm(), parseInt(), parseFloat()
+// () parenthesis
+
+// let fullName = prompt("Enter your full name");
+// console.log(typeof fullName);
+// // alert(fullName);
+// confirm('are you sure you want to Delete?');
+
+
+
+// parseFloat()
+// const number1 = parseFloat(prompt("Enter a number"));
+// let addition = number1 + 10;
+
+// console.log(typeof number1);
+
+// console.log(addition);
+
+console.log(2 ** 5);
+
+// ++(increment), --(decrement)
+
+let number = 10;
+
+number++;
+console.log(number);
+
+
+number--;
+number--;
+
+console.log(number);
+
+
+
+
+
+
+
 
